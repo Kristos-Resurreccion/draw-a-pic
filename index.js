b97b1feafa64c7bbd7b468fb7d78e90d6de0a627
Kristos-Resurreccion <101231670+Kristos-Resurreccion@users.noreply.github.com>
2024-07-05 11:38:34 +1000

    Create Flexible pixel grid with JS

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const canvas = document.querySelector(".canvas")
+const canvas = document.querySelector('.canvas')
 
 
 
 for (i = 0; i <= 15; i++) {
-    const pixel = document.createElement("div")
-    pixel.setAttribute('class', `pixel`)
-    pixel.setAttribute('id', `pixel-${i}`)
-    canvas.append(pixel)
+    const row = document.createElement('div')
+    row.setAttribute('class', 'row')
+    row.setAttribute('id', `row-${i}`)
+    canvas.append(row)
 }
+
+const rows = document.querySelectorAll('.row');
+
+rows.forEach(row => {
+    for (i = 0; i <= 15; i++) {
+        const pixel = document.createElement('div')
+        pixel.setAttribute('class', 'pixel')
+        pixel.setAttribute('id', `pixel${i}`)
+        row.append(pixel)
+    }
+})
+
