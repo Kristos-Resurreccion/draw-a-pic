4f957211ee8d47b998a5122c48c037f6f7da89f7
Kristos-Resurreccion <101231670+Kristos-Resurreccion@users.noreply.github.com>
2024-07-05 12:24:59 +1000

    Turn grid creation into a function and add UI
    Refactor grid creation into a function to allow it to be called
    on button with a prompt, allowing users to customise grid size

--- index.js
+++ index.js
@@ -1,34 +1,66 @@
 const canvas = document.querySelector('.canvas')
 
+function drawGrid(dimension) {
 
+    let rows = document.querySelectorAll('.row');
+    let pixels = document.querySelectorAll('.pixel')
 
-for (i = 0; i <= 15; i++) {
-    const row = document.createElement('div')
-    row.setAttribute('class', 'row')
-    row.setAttribute('id', `row-${i}`)
-    canvas.append(row)
-}
-
-const rows = document.querySelectorAll('.row');
-
-rows.forEach(row => {
-    for (i = 0; i <= 15; i++) {
-        const pixel = document.createElement('div')
-        pixel.setAttribute('class', 'pixel')
-        pixel.setAttribute('id', `pixel${i}`)
-        row.append(pixel)
+    for (i = 0; i < dimension; i++) {
+        const row = document.createElement('div')
+        row.setAttribute('class', 'row')
+        row.setAttribute('id', `row-${i}`)
+        canvas.append(row)
     }
-})
 
+    rows = document.querySelectorAll('.row');
 
-const pixels = document.querySelectorAll('.pixel')
-
-for (i = 0; i < pixels.length; i++) {
-    pixels[i].addEventListener('mouseover', function (e) {
-        let randomHex = (Math.random() * 0xfffff * 1000000).toString(16)
-        const hexColor = '#' + randomHex.slice(0, 6)
-        e.target.style.background = hexColor
+    rows.forEach(row => {
+        for (i = 0; i < dimension; i++) {
+            const pixel = document.createElement('div')
+            pixel.setAttribute('class', 'pixel')
+            pixel.setAttribute('id', `pixel${i}`)
+            row.append(pixel)
+        }
     })
+
+    pixels = document.querySelectorAll('.pixel')
+
+    for (i = 0; i < pixels.length; i++) {
+        pixels[i].addEventListener('mouseover', function (e) {
+            let randomHex = (Math.random() * 0xfffff * 1000000).toString(16)
+            const hexColor = '#' + randomHex.slice(0, 6)
+            e.target.style.background = hexColor
+        })
+    }
 }
 
+drawGrid(16)
+
+
+
+
+
+const gridSizeBtn = document.querySelector('#grid-size')
+gridSizeBtn.addEventListener('click', () => {
+    let promptSize = prompt('Type in your grid size. (max 100, must be a number)');
+    let promptSizeClean = parseInt(promptSize);
+    if (promptSizeClean <= 100) {
+        gridSize = promptSizeClean;
+    } else {
+        gridSize = 100;
+    }
+
+    let rows = document.querySelectorAll('.row');
+    let pixels = document.querySelectorAll('.pixel')
+
+    rows.forEach(row => {
+        row.remove()
+    })
+
+    pixels.forEach(pixel => {
+        pixel.remove()
+    })
+
+    drawGrid(gridSize)
+})
 
